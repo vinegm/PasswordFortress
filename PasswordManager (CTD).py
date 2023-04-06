3a8ae4b59138cb5a044dd109ffbde1a80957b673
vinegm <117782568+vinegm@users.noreply.github.com>
2023-04-06 16:59:43 -0300

    "UI" update

--- PasswordManager (CTD).py
+++ PasswordManager (CTD).py
@@ -5,6 +5,10 @@ class userInfo:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.accounts = dict()
+
+    def getAccounts (self):
+        return self.accounts()
     
     def getUsername (self):
         return self.username
@@ -52,12 +56,15 @@ class usersTable:
             if registerUser.getUsername() == username:
                 del self.users[i][j]
                 break
-        
+
 
 
 users = usersTable()
 while True:
-    action = input("Que ação deseja realizar? \"sair\", \"registrar\", \"entrar\": ").lower()
+    action = input("\nQue ação deseja realizar?\
+                   \n\"Registrar\": Registra um novo usuário.\
+                   \n\"Entrar\": Entra com um usuário existente.\
+                   \n\"Sair\": Fecha o programa.\n").lower()
     
     if action == "sair":
         quit()
@@ -75,6 +82,36 @@ while True:
             password = input("Informe sua senha: ")
             if password == user.password:
                 print("Acesso garantido.")
+                while True:
+                    action = input("\nQue ação deseja realizar?\
+                                    \n\"Ler\": Ver suas contas.\
+                                    \n\"Adicionar\": Adiciona uma nova conta.\
+                                    \n\"Remover\": Remove uma conta existente.\
+                                    \n\"Apagar\": Apaga seu usuário.\
+                                    \n\"Sair\": Sai do seu usuário e renorna ao início.\n").lower()
+                    if action == "ler":
+                        for plataform, plataformPassword in user.accounts.items():
+                            print(f"{plataform}: {plataformPassword}")
+                    if action == "adicionar":
+                        plataform = input("Informe a plataforma: ")
+                        plataformPassword = input("Informe a senha: ")
+                        user.accounts[plataform] = plataformPassword
+                    if action == "remover":
+                        plataform = input("Informe a plataforma que deseja remover: ")
+                        if plataform in user.accounts:
+                            del user.accounts[plataform]
+                        else:
+                            print("Conta inexistente.")
+                    if action == "apagar":
+                        password = input("Informe sua senha para confirmar: ")
+                        if password == user.password:
+                            users.deleteUser(user)
+                            print("Seu usuário foi deletado.")
+                            break
+                        else:
+                            print("Senha incorreta.")
+                    if action == "sair":
+                        break
             else:
                 print("Acesso negado.")
         else:
