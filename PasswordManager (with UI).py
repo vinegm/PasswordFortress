aca534a9beb40021c810d8091a5f76d76be3c9cb
vinegm <117782568+vinegm@users.noreply.github.com>
2023-04-12 20:59:15 -0300

    major changes
    changed the frames from functions to classes, now able to navigate between frames

--- PasswordManager (with UI).py
+++ PasswordManager (with UI).py
@@ -2,90 +2,91 @@ from tkinter import *
 import hashlib
 import pickle
 
-
-def RegisterFrame():
-
-    def RegisterUser(username, password):
-        raise Exception("Code Missing!")
-
-    def CheckForExistingUser(Username):
-        raise Exception("Code Missing!")
-
-    raise Exception("Code Missing!")
-
-def LoginFrame(master):
-    LoginFrame = Frame(master)
-    LoginFrame.pack(anchor="center")
-
-    # Head label
-    head = Label(LoginFrame,
-                 text = "Welcome to a Password Manager!",
-                 font = ("Arial", 12, "bold"))
-    head.grid(row = 0,
-              column = 0, columnspan = 2,
-              sticky="nsew")
-
-    # Username label and entry
-    usernameLabel = Label(LoginFrame,
-                          text = "Username:",
-                          font = ("Arial", 9, "bold"))
-    usernameLabel.grid(pady = 15,
-                       row = 1,
-                       column = 0)
-
-    usernameEntry = Entry(LoginFrame)
-    usernameEntry.grid(pady = 15,
-                       row = 1,
-                       column = 1)
-
-    # Password label and entry
-    passwordLabel = Label(LoginFrame,
-                          text = "Password:",
-                          font = ("Arial", 9, "bold"))
-    passwordLabel.grid(row = 2,
-                       column = 0)
-
-    passwordEntry = Entry(LoginFrame)
-    passwordEntry.grid(row = 2,
-                       column = 1)
-
-    def _GoToRegisterFrame(event):
-        raise Exception("Code Missing!")
-
-    # Label to change frame so the user can register a new profile
-    notRegistered = Label(LoginFrame,
-                          text = "Register",
-                          font = ("Arial", 9, "bold", "underline"),
-                          fg = "Blue")
-    notRegistered.bind("<Button-1>", _GoToRegisterFrame)
-    notRegistered.grid(pady = 5,
-                       row = 3,
-                       column= 0,
-                       sticky = "w")
-
-    # Set focus to the username entry and then to the password entry
-    usernameEntry.focus_set()
-    def _FocusPasswordEntry(event):
-        passwordEntry.focus_set()
-    usernameEntry.bind("<Return>", _FocusPasswordEntry)
-
-    # Returns the username and password
-    def _LoginUser(event):
-        username = hashlib.md5(usernameEntry.get().encode()).hexdigest()
-        password = hashlib.md5(passwordEntry.get().encode()).hexdigest()
-        print(f"user: {username}")
-        print(f"password: {password}")
+class PasswordManager (Tk):
+    def __init__(self):
+        Tk.__init__(self)
         
-        raise Exception("Missing Code!")
-    passwordEntry.bind("<Return>", _LoginUser)
+        framesHolder = Frame(self)
+        framesHolder.pack(side="top", fill="both", expand=True)
+        framesHolder.grid_rowconfigure(0, weight=1)
+        framesHolder.grid_columnconfigure(0, weight=1)
+        
+        self.frames = {}
+        for F in (LoginFrame, RegisterFrame, ProfileFrame):
+            page_name = F.__name__
+            frame = F(framesHolder, self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
     
+        self.ChangeFrame("LoginFrame")
 
-if __name__ == "__main__":
-    window = Tk()
-    window.geometry("750x250")
-    window.title("Password Manager")
-    LoginFrame(window)
+    def ChangeFrame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
 
+class LoginFrame (Frame):
+    def __init__(self, master, controller):
+        Frame.__init__(self, master)
+        head = Label(self,
+                     text = "Welcome to a Password Manager!",
+                     font = ("Arial", 12, "bold"))
+        head.grid(row = 0,
+                  column = 0, columnspan = 2,
+                  sticky="nsew")
+
+        # Username label and entry
+        usernameLabel = Label(self,
+                              text = "Username:",
+                              font = ("Arial", 9, "bold"))
+        usernameLabel.grid(pady = 15,
+                           row = 1,
+                           column = 0)
+
+        usernameEntry = Entry(self)
+        usernameEntry.grid(pady = 15,
+                           row = 1,
+                           column = 1)
+
+        # Password label and entry
+        passwordLabel = Label(self,
+                              text = "Password:",
+                              font = ("Arial", 9, "bold"))
+        passwordLabel.grid(row = 2,
+                        column = 0)
+
+        passwordEntry = Entry(self)
+        passwordEntry.grid(row = 2,
+                           column = 1)
+        
+        notRegistered = Label(self,
+                              text = "Register",
+                              font = ("Arial", 9, "bold", "underline"),
+                              fg = "Blue")
+        notRegistered.bind("<Button-1>", lambda event: controller.ChangeFrame("RegisterFrame"))
+        notRegistered.grid(pady = 5,
+                           row = 3,
+                           column= 0,
+                           sticky = "w")
+        
+        def _FocusPasswordEntry(event):
+            passwordEntry.focus_set()
+        usernameEntry.bind("<Return>", _FocusPasswordEntry)
+        def _LoginUser(event):
+            username = hashlib.md5(usernameEntry.get().encode()).hexdigest()
+            password = hashlib.md5(passwordEntry.get().encode()).hexdigest()
+            print(f"user: {username}")
+            print(f"password: {password}")
+            
+            controller.ChangeFrame("ProfileFrame")
+        passwordEntry.bind("<Return>", _LoginUser)        
+class RegisterFrame (Frame):
+    def __init__(self, master, controller):
+        Frame.__init__(self, master)
+
+class ProfileFrame (Frame):
+    def __init__(self, master, controller):
+        Frame.__init__(self, master)
+
+if __name__ == '__main__':
+    window = PasswordManager()
     window.mainloop()
-
-print("finalizado")
