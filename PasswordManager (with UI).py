71d05d5c41fc2f5f9c822d4a93555cb40b813f0b
vinegm <117782568+vinegm@users.noreply.github.com>
2023-04-12 18:09:19 -0300

    Update PasswordManager (with UI).py

--- PasswordManager (with UI).py
+++ PasswordManager (with UI).py
@@ -19,14 +19,16 @@ def LoginFrame(master):
 
     # Head label
     head = Label(LoginFrame,
-                 text = "Welcome to a Password Manager!")
+                 text = "Welcome to a Password Manager!",
+                 font = ("Arial", 12, "bold"))
     head.grid(row = 0,
               column = 0, columnspan = 2,
               sticky="nsew")
 
     # Username label and entry
     usernameLabel = Label(LoginFrame,
-                          text = "Username: ")
+                          text = "Username:",
+                          font = ("Arial", 9, "bold"))
     usernameLabel.grid(pady = 15,
                        row = 1,
                        column = 0)
@@ -38,13 +40,28 @@ def LoginFrame(master):
 
     # Password label and entry
     passwordLabel = Label(LoginFrame,
-                          text = "Password: ")
+                          text = "Password:",
+                          font = ("Arial", 9, "bold"))
     passwordLabel.grid(row = 2,
                        column = 0)
 
     passwordEntry = Entry(LoginFrame)
     passwordEntry.grid(row = 2,
                        column = 1)
+
+    def _GoToRegisterFrame(event):
+        raise Exception("Code Missing!")
+
+    # Label to change frame so the user can register a new profile
+    notRegistered = Label(LoginFrame,
+                          text = "Register",
+                          font = ("Arial", 9, "bold", "underline"),
+                          fg = "Blue")
+    notRegistered.bind("<Button-1>", _GoToRegisterFrame)
+    notRegistered.grid(pady = 5,
+                       row = 3,
+                       column= 0,
+                       sticky = "w")
 
     # Set focus to the username entry and then to the password entry
     usernameEntry.focus_set()
@@ -58,6 +75,7 @@ def LoginFrame(master):
         password = hashlib.md5(passwordEntry.get().encode()).hexdigest()
         print(f"user: {username}")
         print(f"password: {password}")
+        
         raise Exception("Missing Code!")
     passwordEntry.bind("<Return>", _LoginUser)
     
