1c92caccd638b477f83ed4ad092161752b47320e
vinegm <117782568+vinegm@users.noreply.github.com>
2023-04-12 16:46:56 -0300

    Doing Login/Register frame

--- PasswordManager (with UI).py
+++ PasswordManager (with UI).py
@@ -6,30 +6,41 @@ def LoginRegisterFrame(master):
     LoginRegisterFrame = Frame(master)
     LoginRegisterFrame.pack(anchor="center")
 
-    # Label and entry for Login/Register
-
     # Username label and entry
     usernameLabel = Label(LoginRegisterFrame,
-                   text = "Usuário: ")
+                          text = "Usuário: ")
     usernameLabel.grid(row=0, column=0)
 
-    userEntry = Entry(LoginRegisterFrame)
-    userEntry.grid(pady=15, row=0, column=1)
+    usernameEntry = Entry(LoginRegisterFrame)
+    usernameEntry.grid(pady=25, row=0, column=1)
 
     # Password label and entry
     passwordLabel = Label(LoginRegisterFrame,
-                   text = "Senha: ")
+                          text = "Senha: ")
     passwordLabel.grid(row=1, column=0)
 
     passwordEntry = Entry(LoginRegisterFrame)
     passwordEntry.grid(row=1, column=1)
 
+    usernameEntry.focus_set()
+    def _FocusPasswordEntry(event):
+        passwordEntry.focus_set()
+    usernameEntry.bind("<Return>", _FocusPasswordEntry)
 
+    def _Return(event):
+        username = usernameEntry.get()
+        password = passwordEntry.get()
+        print(f"user: {username}")
+        print(f"password: {password}")
+    passwordEntry.bind("<Return>", _Return)
+    
 
-window = Tk()
-window.title("Password Manager")
-LoginRegisterFrame(window)
+if __name__ == "__main__":
+    window = Tk()
+    window.geometry("750x250")
+    window.title("Password Manager")
+    LoginRegisterFrame(window)
 
-window.mainloop()
+    window.mainloop()
 
 print("finalizado")
